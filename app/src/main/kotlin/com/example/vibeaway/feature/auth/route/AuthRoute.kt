2ad8562fd0fd2dfa4109e5c1fe395e10c9da6a4b
Semenciuc, Cosmin <cosmin.semenciuc@garmin.com>
2025-05-19 10:20:38 +0300

    Remove testing line

--- app/src/main/kotlin/com/example/vibeaway/feature/auth/route/AuthRoute.kt
+++ app/src/main/kotlin/com/example/vibeaway/feature/auth/route/AuthRoute.kt
@@ -23,8 +23,6 @@ fun AuthRoute() {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val activity = LocalActivity.current as? AuthActivity
 
-    activity?.let(OnboardingActivity::startActivity)
-
     AuthScreen(
         modifier = Modifier.imePadding(),
         authScreenType = uiState.authScreenType,
